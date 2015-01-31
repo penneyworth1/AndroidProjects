9ef6c629d7eacabcb566b165f232d0e631dc9b4b
Steven Stewart <stevenstewart@Stevens-MacBook-Pro-3.local>
2015-01-31 14:12:26 -0600

    new proj

--- ogl2spritetest/app/src/main/java/com/test/ogl2spritetest/MainRenderer.java
+++ ogl2spritetest/app/src/main/java/com/test/ogl2spritetest/MainRenderer.java
@@ -96,15 +96,6 @@ public class MainRenderer implements GLSurfaceView.Renderer
 
             b1.recycle();
 
-
-//            gl.glClientActiveTexture(GL10.GL_TEXTURE0);
-//            gl.glBindTexture(GL10.GL_TEXTURE_2D, mTextureList[0]);
-//            gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
-//            gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
-//            gl.glTexEnvf(GL10.GL_TEXTURE_ENV, GL10.GL_TEXTURE_ENV_MODE, GL10.GL_MODULATE);
-//            gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-//            GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, b1, 0);
-
             Log.d("", "");
         }
         catch (Exception ex)
